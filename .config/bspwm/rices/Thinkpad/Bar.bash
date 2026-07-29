8eb818539e2189b8bdaf04a2251232d3220df466
ffgtfgh53 <152868614+ffgtfgh53@users.noreply.github.com>
2026-07-29 09:02:33 +0800

    Update polybar and others

--- .config/bspwm/rices/Thinkpad/Bar.bash
+++ .config/bspwm/rices/Thinkpad/Bar.bash
@@ -2,11 +2,10 @@
 for mon in $(polybar --list-monitors | cut -d":" -f1); do
 	[[ $mon = $(cat "${HOME}/.config/bspwm/src/config/.no_polybar_mon") ]] && continue
 	(
-        MONITOR=$mon polybar pam1 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-	MONITOR=$mon polybar pam2 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-	MONITOR=$mon polybar pam3 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-	MONITOR=$mon polybar pam4 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-	MONITOR=$mon polybar pam5 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-	MONITOR=$mon polybar pam6 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
+    MONITOR=$mon polybar bar1 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
+	MONITOR=$mon polybar bar2 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
+	MONITOR=$mon polybar bar3 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
+	MONITOR=$mon polybar bar4 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
+	MONITOR=$mon polybar bar5 -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
     )
 done
